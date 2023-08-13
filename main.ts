bcab7c97b6594ae27fc2f37d6216115362e4266a
Dalkson <Jacksondpope@gmail.com>
2023-08-13 11:09:18 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 const height: number = 780;
-const width: number = 900;
+const width: number = 1000;
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 canvas.width = width;
 canvas.height = height;
@@ -8,9 +8,15 @@ const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 let currentTextState: string = "Not in Room";
 let currentRoom: number = 0;
 
-let playerNumber = 0;
+const rowGap = 70;
+const colors = ["#FFFFFF", "#FF0000", "#FBFF00", "orange", "#04857F", "#359A2E", "magenta"]
 
-import { defaultMaxListeners } from "events";
+let playerNumber = 0;
+let usernameList: string[] = [];
+
+document.getElementById("rematchButton").style.display = 'none';
+document.getElementById("startButton").style.display = 'none';
+
 import { io } from "socket.io-client";
 
 const socket = io("http://75.136.67.139:1234");
@@ -36,14 +42,23 @@ socket.on("joined", (room: number, player: number) => {
   currentRoom = room;
   playerNumber = player;
   clearBoard();
+  lobbyUI()
 });
 
-socket.on("gamestart", () => {
-  playerNumber == 1 ? currentTextState = "Your Turn" : currentTextState = "Waiting";
+socket.on("players", (userList: string[]) => {
+  usernameList = userList;
+})
+
+socket.on("gamestart", (turn: number) => {
+  gameStartUI()
+  playerNumber == turn ? currentTextState = "Your Turn" : currentTextState = "Player" + turn + "'s Turn";
 })
 
 socket.on("gameover", (player: number) => {
-  switch (player) {
+  document.getElementById("rematchButton").style.display = '';
+  console.log("game is over " + player);
+
+  setTimeout(() => {  switch (player) {
     case -1:
       currentTextState = "Stalemate!";
       break;
@@ -53,49 +68,52 @@ socket.on("gameover", (player: number) => {
     default:
       currentTextState = "You Lose!";
       break;
-  }
+  }}, 100);
+
 })
+
+
 
 socket.on("rematch", (turn: number) => {
+  document.getElementById("rematchButton").style.display = 'none';
+  playerNumber == turn ? currentTextState = "Your Turn" : currentTextState = "Player" + turn + "'s Turn";
   clearBoard();
-  playerNumber == turn ? currentTextState = "Your Turn" : currentTextState = "Waiting";
 })
 
-socket.on("droppedDisk", (player: number, column: number) => {
+socket.on("droppedDisk", (player: number, column: number, turn: number) => {
+  if (playerNumber == turn) {currentTextState = "Your Turn!";} else {
+    currentTextState = "Player" + turn + "'s Turn";}
   dropDisk(player, column);
-  switch (playerNumber) {
-    case 1:
-      if (player == 3) {
-        currentTextState = "Your Turn";
-      } else {currentTextState = "Waiting";}
-      break;
-    case 2:
-      if (player == 1) {
-        currentTextState = "Your Turn";
-      } else {currentTextState = "Waiting";}
-      break;
-    case 3:
-      if (player == 2) {
-        currentTextState = "Your Turn";
-      } else {currentTextState = "Waiting";}
-      break;
-  }
 })
+
 
 let joinButton = document.getElementById("joinButton");
 joinButton.addEventListener("click", (e:Event) => {
-  var inputValue = Number((<HTMLInputElement>document.getElementById("joinID")).value);
-  socket.emit("join", inputValue);
+  var roomCode = Number((<HTMLInputElement>document.getElementById("joinID")).value);
+  var username = String((<HTMLInputElement>document.getElementById("username")).value);
+  if (username == '') {
+    currentTextState = "Enter Name"
+  } else{socket.emit("join", roomCode, username);}
 });
 
 let hostButton = document.getElementById("hostButton");
 hostButton.addEventListener("click", (e:Event) => {
-  socket.emit("host");
+  var username = String((<HTMLInputElement>document.getElementById("username")).value);
+  if (username == '') {
+    currentTextState = "Enter Name"
+  } else{
+    socket.emit("host", (username));
+    document.getElementById("startButton").style.display = '';}
 });
 
 let rematchButton = document.getElementById("rematchButton");
 rematchButton.addEventListener("click", (e:Event) => {
   socket.emit("rematch", (currentRoom));
+});
+
+let startButton = document.getElementById("startButton");
+startButton.addEventListener("click", (e:Event) => {
+  socket.emit("startGame", (currentRoom));
 });
 
 let board: number[][] = [
@@ -112,64 +130,28 @@ let board: number[][] = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
-const mousePosText = document.getElementById('mouse-pos');
-let mousePos = {x: 1, y: 1};
 
-window.addEventListener('mousemove', (event) => {
-  mousePos = { x: event.clientX, y: event.clientY };
-  mousePosText.textContent = `(${mousePos.x}, ${mousePos.y})`;
-});
 
 function printMousePos(event: MouseEvent): void {
   if (event.clientY >= 100 && event.clientY <= 780 && event.clientX <= 850 && event.clientX >= 60) {
     let column = 0
-    if (event.clientX >= 70 && event.clientX <= 140) {
-      column = 0;
-    }
-    if (event.clientX >= 141 && event.clientX <= 210) {
-      column = 1;
-    }
-    if (event.clientX >= 211 && event.clientX <= 280) {
-      column = 2;
-    }
-    if (event.clientX >= 281 && event.clientX <= 350) {
-      column = 3;
-    }
-    if (event.clientX >= 351 && event.clientX <= 420) {
-      column = 4;
-    }
-    if (event.clientX >= 421 && event.clientX <= 490) {
-      column = 5;
-    }
-    if (event.clientX >= 491 && event.clientX <= 560) {
-      column = 6;
-    }
-    if (event.clientX >= 561 && event.clientX <= 630) {
-      column = 7;
-    }
-    if (event.clientX >= 631 && event.clientX <= 700) {
-      column = 8;
-    }
-    if (event.clientX >= 701 && event.clientX <= 770) {
-      column = 9;
-    }
-    if (event.clientX >= 771 && event.clientX <= 840) {
-      column = 10;
-    }
-    if (isFull(board[column])) {
-      return;
+    for (let c = 0; c < board.length; c++) {
+      if (event.clientX >= (rowGap * (c + 1) + 1) && event.clientX <= (rowGap * 2) * (c + 1)) {
+        column = c;
+      }
     }
     socket.emit("dropDisk", column, currentRoom)
-  }  
+  }
 }
-  
-  document.addEventListener("click", printMousePos);
+
+document.addEventListener("click", printMousePos);
 
 function main(): void {
   function draw_frame(): void {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBoard();
     drawText(currentTextState);
+    drawPlayers(usernameList);
 
     window.requestAnimationFrame(draw_frame);
   }
@@ -177,43 +159,43 @@ function main(): void {
 }
 
 function drawText(text: string): void {
-  ctx.fillStyle = "black"
-  ctx.fillRect(230, 20, 290, 75)
-  ctx.fillStyle = "red"
+  ctx.fillStyle = "black";
+  ctx.fillRect(230, 20, 320, 75);
+  ctx.fillStyle = "red";
   ctx.font = `40px Verdana`;
   ctx.fillText(text, 250, 70);
 }
 
+function drawPlayer(username: string, i: number): void {
+  ctx.fillStyle = colors[i+1];
+  ctx.fillRect(840, 150 + (i * 70), 150, 60);
+  ctx.fillStyle = "black";
+  ctx.font = `30px Verdana`;
+  ctx.fillText(username, 850, 190 + (i * 70));
+  
+}
+
+function drawPlayers(usernameList: string[]) {
+  for (let i = 0; i < usernameList.length; i++) {
+    drawPlayer(usernameList[i], i)
+  }
+}
+
 function drawBoard(): void {
-  ctx.fillStyle = "blue";
-  ctx.fillRect(0, 0, 1100, 780);
+  ctx.fillStyle = "#0B0B6F";
+  ctx.fillRect(0, 0, 1500, 780);
   let x = 100;
   board.forEach(column => {
     let y = 175;
     column.forEach(cell => {
-      switch(cell) {
-        case 1:
-          drawCircle(x, y, 30, "red");
-          break;
-        case 2:
-          drawCircle(x, y, 30, "yellow");
-          break;
-        case 3:
-          drawCircle(x, y, 30, "green");
-          break;
-        default:
-          drawCircle(x, y, 30, "white");
-          break;
-      }
-      y += 70;
+      drawCircle(x, y, 30, colors[cell])
+      y += rowGap;
     });
-    x += 70;
+    x += rowGap;
   });
 }
 
-function isFull(column: number[]): boolean {
-  if (column[0] == 0) {return false;} else {return true;}
-}
+
 
 function dropDisk(player: number, column: number): void {
   for (let index = board[column].length - 1; index >= 0; index--) {
@@ -246,6 +228,23 @@ function clearBoard() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
+}
+
+function gameStartUI() {
+  document.getElementById("joinButton").style.display = 'none';
+  document.getElementById("hostButton").style.display = 'none';
+  document.getElementById("joinID").style.display = 'none';
+  document.getElementById("username").style.display = 'none';
+  document.getElementById("rematchButton").style.display = 'none';
+  document.getElementById("startButton").style.display = 'none';
+}
+
+function lobbyUI() {
+  document.getElementById("joinButton").style.display = 'none';
+  document.getElementById("hostButton").style.display = 'none';
+  document.getElementById("joinID").style.display = 'none';
+  document.getElementById("username").style.display = 'none';
+  document.getElementById("rematchButton").style.display = 'none';
 }
 
 main()
